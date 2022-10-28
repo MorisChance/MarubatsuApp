85b7e99414236cadbcbdd419b1837d1d055054f4
Moris8181 <moriskousuke8181@gmail.com>
2022-10-28 23:31:00 +0900

    ○☓アプリ完成

--- MarubatsuApp/ViewController.swift
+++ MarubatsuApp/ViewController.swift
@@ -8,72 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var questionLabel: UILabel!
     var currentQuestionNum : Int = 0
-    // 問題
-    var questions: [[String: Any]] = [
-        [
-            "question": "iPhoneアプリを開発する統合環境はZcodeである",
-            "answer": false
-        ],
-    ]
-    //呼び出しメソッド
-    func calling() {
-        let morisDefaults = UserDefaults.standard
-        if morisDefaults.object(forKey: "add") != nil {
-            questions = morisDefaults.object(forKey: "add") as! [[String: Any]]
-        }
-        showQuestion()
-    }
+    // 問題の配列を用意
+    var questions: [[String: Any]] = []
     //問題を表示する関数
     func showQuestion(){
         let question = questions[currentQuestionNum]
         if let que = question["question"] as? String{
             questionLabel.text = que
+            print(que)
         }
     }
     // 回答をチェックする関数
     // 正解なら次の問題を表示します
     func checkAnswer(yourAnswer: Bool) {
-
         let question = questions[currentQuestionNum]
-
-        if let ans = question["answer"] as? Bool {
-
-            if yourAnswer == ans {
-                // 正解
-                // currentQuestionNumを1足して次の問題に進む
-                currentQuestionNum += 1
-                showAlert(message: "正解！")
+        if questions.isEmpty != true{
+            if let ans = question["answer"] as? Bool {
+                if yourAnswer == ans {
+                    // 正解
+                    // currentQuestionNumを1足して次の問題に進む
+                    currentQuestionNum += 1
+                    showAlert(message: "正解！")
+                } else {
+                    // 不正解
+                    showAlert(message: "不正解だよーん")
+                }
             } else {
-                // 不正解
-                showAlert(message: "不正解だよーん")
+                print("答えが入ってません")
+                return
             }
-        } else {
-            print("答えが入ってません")
-            return
+            if currentQuestionNum >= questions.count{
+                currentQuestionNum = 0
+            }
+            // 問題を表示します。
+            // 正解であれば次の問題が、不正解であれば同じ問題が再表示されます。
+            showQuestion()
         }
-        if currentQuestionNum >= questions.count{
-            currentQuestionNum = 0
+        
+        func showAlert(message: String){
+            let alert = UIAlertController(title:nil, message:message, preferredStyle: .alert)
+            let close = UIAlertAction(title:"閉じる", style: .cancel, handler:nil)
+            alert.addAction(close)
+            present(alert, animated: true, completion:nil)
         }
-        // 問題を表示します。
-        // 正解であれば次の問題が、不正解であれば同じ問題が再表示されます。
-        showQuestion()
+        func viewDidLoad() {
+            super.viewDidLoad()
+        }
     }
-    
-    func showAlert(message: String){
-        let alert = UIAlertController(title:nil, message:message, preferredStyle: .alert)
-        let close = UIAlertAction(title:"閉じる", style: .cancel, handler:nil)
-        alert.addAction(close)
-        present(alert, animated: true, completion:nil)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        showQuestion()
-    }
-
     @IBAction func tappedNoButton(_ sender: Any) {
         checkAnswer(yourAnswer: false)
     }
@@ -81,6 +65,16 @@ class ViewController: UIViewController {
     @IBAction func tappedYesButton(_ sender: Any) {
         checkAnswer(yourAnswer: true)
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+
+        if UserDefaults.standard.array(forKey: "add")?.first != nil {
+            questions = UserDefaults.standard.array(forKey: "add") as! [[String:Any]]
+            showQuestion()
+        } else {
+            questionLabel.text = "問題を入力してください"
+        }
+        
+    }
 }
+
 
--- MarubatsuApp/addNewQuestionViewController.swift
+++ MarubatsuApp/addNewQuestionViewController.swift
@@ -1,4 +1,4 @@
-        //
+//
 //  addNewQuestionViewController.swift
 //  MarubatsuApp
 //
@@ -15,7 +15,16 @@ class addNewQuestionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        }
+    //アラートのメッソドを作成
+    func showAlert(message: String){
+        let alert = UIAlertController(title:nil, message:message, preferredStyle: .alert)
+        let close = UIAlertAction(title:"閉じる", style: .cancel, handler:nil)
+        alert.addAction(close)
+        present(alert, animated: true, completion:nil)
+        
     }
+
     //トップ画面に戻る
     @IBAction func returnButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
@@ -23,38 +32,44 @@ class addNewQuestionViewController: UIViewController {
     //保存ボタンと紐付け
     @IBAction func saveButton(_ sender: Any) {
         //もし問題文が空であれば
-            if newQuestionText.text?.isEmpty ?? true {
-                print("問題文は空白です。")
+        if newQuestionText.text?.isEmpty ?? true {
+            showAlert(message: "問題を入力してください")
+        } else {
+            var answer : Bool
+            if segmetedControl.selectedSegmentIndex == 0{
+                answer = false
             } else {
-                var answer : Bool
-                if segmetedControl.selectedSegmentIndex == 0{
-                    answer = false
-                } else {
-                    answer = true
-                }
-
-            //userDefaultsのインスタンス生成
-                let morisDefaults = UserDefaults.standard
-                if morisDefaults.object(forKey: "add") != nil{
-                    questions = morisDefaults.object(forKey: "add") as! [[String: Any]]
-                }
-            //questionsに問題文と○☓を追加
-                questions.append(["question":newQuestionText!, "answer": answer])
-            //questionsに入った配列を保存
-                morisDefaults.set( questions, forKey : "add")
-                print("questions")
+                answer = true
             }
+            
+            //userDefaultsのインスタンス生成
+            let morisDefaults = UserDefaults.standard
+            if morisDefaults.object(forKey: "add") != nil{
+                questions = morisDefaults.object(forKey: "add") as! [[String: Any]]
+            }
+            //questionsに問題文と○☓を追加
+            questions.append(["question":newQuestionText.text!, "answer": answer])
+            //questionsに入った配列を保存
+            morisDefaults.set( questions, forKey : "add")
+            newQuestionText.text = ""
         }
+    }
     //入力を終える
     @IBAction func end(_ sender: Any) {
     }
+    //問題削除ボタン
+    @IBAction func clearData(_ sender: Any) {
+        let appDomain = Bundle.main.bundleIdentifier
+        UserDefaults.standard.removePersistentDomain(forName: appDomain!)
+        showAlert(message:"問題を全て削除しました")
+    }
     /*
-            // MARK: - Navigation
-            
-            // In a storyboard-based application, you will often want to do a little preparation before navigation
-            override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            // Get the new view controller using segue.destination.
-            // Pass the selected object to the new view controller.
-            }
-            */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
